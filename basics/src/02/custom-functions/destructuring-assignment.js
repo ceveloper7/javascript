b76ebfb8a356d91e4c2b00914748d50177b33b3e
Carlos V. <cevabarcvilla@gmail.com>
2025-03-27 19:29:09 -0300

    Getting Started Javascript - chapter 2 - Custom Functions - Destructuring assignment

---
+++ basics/src/02/custom-functions/destructuring-assignment.js
@@ -0,0 +1,23 @@
+(() => {
+  /**
+   * Las funcion proveen un valor de retorno. Si queremos usar una funcion para retornar multiples valores o cambiar multiples valores transferidos, tenemos dos opciones:
+   * Podemos guardar los valores en un campo y transferirlo o retornar el campo
+   * Podemos emplear destructuring asignment 
+   */
+  function swap(x, y) {
+    document.write("After the transfer: " + x + " " + y + "<br> ");
+    const temp = x;
+    x = y;
+    y = temp;
+    document.write("Before the return: " + x + " " + y + "<br>");
+
+    return [x, y];
+  }
+  
+  let a = 7;
+  let b = 12;
+
+  document.write("Before the swap: " + a + " " + b + "<br>"); 
+  [p, q] = swap(a, b);
+  document.write("After the swap: " + p + " " + q);
+})();
